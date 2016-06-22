c666a080e7aaf78d24d83945f9e79f55be5fb964
scott holland <scotthollandjr@gmail.com>
2016-06-22 09:16:28 -0700

    Fix repeating random number loop

--- app/components/reset-game.js
+++ app/components/reset-game.js
@@ -3,27 +3,22 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     reset(user) {
-      var confirmed = true;
-      // var confirmed = confirm("Are you sure you'd like to reset the game? All user information will be erased.");
+      var confirmed = confirm("Are you sure you'd like to reset the game? All user information will be erased.");
       if (confirmed) {
         var empty = [];
         var randoms = [];
         var users = this.get('user');
         var userNames = users.getEach('username');  //returns array of usernames
-        var numUsers = (userNames.length);  //returns 19 for now
-        for (var i = 0; i = numUsers; i++) {
-          var random = Math.floor(Math.random() * (numUsers - 0) + 0);
-            while ((randoms.indexOf(random)) === -1) {
+        var numUsers = userNames.length;  //returns 19 for now
+
+         while (randoms.length < numUsers) {
+          var random = Math.floor(Math.random() * (numUsers) + 0);
+            if (randoms.indexOf(random) === -1) {
               randoms.push(random);
-              console.log(random);
+              
             }
           }
-        //
-        // userNames.foreach(function(user) {
-        //   var random = Math.random() * (numUsers - ) + min;
-        // }); console.log(userArray);
-        //
-        // alert("Game has been reset.");
+        alert("Game has been reset.");
       }
     }
   }
