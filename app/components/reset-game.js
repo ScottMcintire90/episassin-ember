60d3907d5605258cff58d96ba6e957b547c6db72
scott holland <scotthollandjr@gmail.com>
2016-06-22 11:32:59 -0700

    Finalize target/user relationship generation

--- app/components/reset-game.js
+++ app/components/reset-game.js
@@ -3,8 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     reset(user, target) {
-      var confirmed = true;
-      //var confirmed = confirm("Are you sure you'd like to reset the game? All user information will be erased.");
+      var confirmed = confirm("Are you sure you'd like to reset the game? All user information will be erased.");
       if (confirmed) {
         var randoms = [];
         var users = this.get('user');
@@ -17,24 +16,14 @@ export default Ember.Component.extend({
               randoms.push(random);
             }
           }
-        //alert("Game has been reset.");
-        console.log(randoms);
         for(var i = 0; i < randoms.length; i++){
           user.findBy("id", "" + i + "").set('target', target.findBy("id", "" + randoms[i] + ""));
+          user.findBy("id", "" + i + "").set('kill', "0");
           user.findBy("id", "" + i + "").save();
-          console.log(randoms[i]);
           target.findBy("id", "" + randoms[i] + "").set('user', user.findBy("id", "" + i + ""));
           target.findBy("id", "" + randoms[i] + "").save();
         }
       }
-
     }
-
-
   }
 });
-
-// user.findBy("id", "3").get('username');
-
-//var userNames = users.getEach('username');
-//var numUsers = userNames.length;
