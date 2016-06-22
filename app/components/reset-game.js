deb61e3a9410f7ca7f24a3c613e81391f89080ec
scott mcintire <scottmcintire90@gmail.com>
2016-06-22 10:04:03 -0700

    add user-detail page styling and content

--- app/components/reset-game.js
+++ app/components/reset-game.js
@@ -3,31 +3,23 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     reset(user) {
-      var confirmed = true;
       // var confirmed = confirm("Are you sure you'd like to reset the game? All user information will be erased.");
-      if (confirmed) {
-        var empty = [];
         var randoms = [];
         var users = this.get('user');
         var userNames = users.getEach('username');  //returns array of usernames
         var numUsers = (userNames.length);  //returns 19 for now
-        for (var i = 0; i = numUsers; i++) {
-          var random = Math.floor(Math.random() * (numUsers - 0) + 0);
-            while ((randoms.indexOf(random)) === -1) {
-              randoms.push(random);
-              console.log(random);
-            }
-          }
-        //
-        // userNames.foreach(function(user) {
-        //   var random = Math.random() * (numUsers - ) + min;
-        // }); console.log(userArray);
-        //
-        // alert("Game has been reset.");
+        for (var i = 0; i < numUsers; i++) {
+          randoms.push(i);
+        };
+        console.log(randoms);
+        for(randoms; x=randoms.length; x--) {
+          var newrandom = randoms.splice(Math.floor(Math.random() * (x + 1)), 1)[0];
+        };
+        console.log(newrandom);
       }
     }
-  }
 });
+
 
 //var userNames = users.getEach('username');
 //var numUsers = userNames.length;
--- app/models/user.js
+++ app/models/user.js
@@ -8,5 +8,6 @@ export default Model.extend({
   avatar: attr(),
   target: belongsTo('target', { async:true }),
   kill: attr(),
-  code: attr()
+  code: attr(),
+  weapon: attr()
 });
