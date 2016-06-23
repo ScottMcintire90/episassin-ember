2820dd8e8468555f40be1b2c3f11950b43dac5be
scott holland <scotthollandjr@gmail.com>
2016-06-23 15:24:06 -0700

    hail mary push before it all breaks

--- app/components/user-detail.js
+++ app/components/user-detail.js
@@ -5,6 +5,15 @@ export default Ember.Component.extend({
   model(params) {
     return this.store.findRecord('user', params.user_id);
   },
+  trophy: Ember.computed('user.kill', function() {
+    var kills = parseInt(this.get('user.kill'));
+    var trophies = [];
+
+    for(var i = 0; i < kills; i++) {
+      trophies.push("cheese");
+    };
+    return trophies;
+  }),
   actions: {
     killUser(user) {
       this.set('user.dead', true);
