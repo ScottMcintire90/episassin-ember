b481c464e767dfb304c51a9d400bca4c19d2ac34
scott holland <scotthollandjr@gmail.com>
2016-06-23 13:37:26 -0700

    once more with feeling

---
+++ tests/unit/helpers/dead-face-test.js
@@ -0,0 +1,10 @@
+import { deadFace } from 'episassin/helpers/dead-face';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | dead face');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = deadFace([42]);
+  assert.ok(result);
+});
